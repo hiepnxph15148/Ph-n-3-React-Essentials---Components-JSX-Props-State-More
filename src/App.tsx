7cf692729e4ce0e67f3da271220ed541a1ec1fbe
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-09 13:16:36 +0700

    new data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Component/Layout/Header'
-
-
-
+import {useState} from "react";
+import "./App.css";
+import componentImage from "../src/assets/react.svg";
+import { CORE_CONCEPTS } from './main';  
+import Header from "./Component/Layout/Header";
+function CoreContempt(props: Props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-      <Header />
-
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+        <Header />
+        <main>
+          <h2>Core Component</h2>
+          <selection id="core-concepts">
+            <ul>
+              <CoreContempt
+                title={CORE_CONCEPTS[0].title}
+                description="A component is a reusable building block in React."
+                image={componentImage}
+              />
+              <CoreContempt
+                title="Component"
+                description="A component is a reusable building block in React."
+                image={componentImage}
+              />{" "}
+              <CoreContempt
+                title="Component"
+                description="A component is a reusable building block in React."
+                image={componentImage}
+              />{" "}
+              <CoreContempt
+                title="Component"
+                description="A component is a reusable building block in React."
+                image={componentImage}
+              />
+            </ul>
+          </selection>
+        </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Layout/Header.tsx
+++ src/Component/Layout/Header.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import reactImage from "../../assets/react.svg";
+const reactDescriptions = ["Fundamentals", "Crucials", "Core"];
 
-const reactDescriptons = ['Fundamentals','Crucials','Core']
-
-function getRamdomInt(max){
-    return Math.floor(Math.random() * (max+1));
+function getRadomInt(max: number) {
+  return Math.floor(Math.random() * (max + 1));
 }
-const descriptino =reactDescriptons[getRamdomInt(2)]
+const description = reactDescriptions[getRadomInt(2)];
 
-function Header () {
-    return(
-         <header>
-           <img src="/src/assets/react1.svg" alt="Stylized atom" />
-           <h1>React Essentials</h1>
-           <p>
-             {descriptino} React concepts you will need for almost any app you are
-             going to build!
-           </p>
-         </header>
-         )
-       }
 
-export default Header
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img src={reactImage} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+     
+    </>
+  );
+}
+
+export default Header;
